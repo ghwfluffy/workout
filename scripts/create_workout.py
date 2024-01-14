a7f8fdfb8208d3763d489c20915393e937e93c9b
GhwFluffy <ghw@ghwfluffy.com>
2024-01-14 10:46:06 -0600

    * Adding yesterday's workout data * Adding extra metadata to exercises * Adding stretching to create workout script

--- scripts/create_workout.py
+++ scripts/create_workout.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import yaml
+import random
 import argparse
 
 from utils.data import get_data
@@ -8,33 +8,41 @@ from utils.data import get_data
 #from datetime import date, datetime
 #from utils.datemanip import today, DateDelta, period_to_month_multiplier
 
+# XXX: Incorporate better
+exclude = [
+    "Front Squats",
+    "Man Makers",
+    "Wall Balls",
+]
+
 # Create the parser
-parser = argparse.ArgumentParser(
-    description="Create workout.")
+def get_args():
+    parser = argparse.ArgumentParser(
+        description="Create workout.")
 
-# Add arguments
-parser.add_argument(
-    "-U",
-    "--upper-body",
-    action="store_true",
-    )
+    # Add arguments
+    parser.add_argument(
+        "-U",
+        "--upper-body",
+        action="store_true",
+        )
 
-parser.add_argument(
-    "-L",
-    "--lower-body",
-    action="store_true",
-    )
+    parser.add_argument(
+        "-L",
+        "--lower-body",
+        action="store_true",
+        )
 
-parser.add_argument(
-    "-F",
-    "--full-body",
-    action="store_true",
-    )
+    parser.add_argument(
+        "-F",
+        "--full-body",
+        action="store_true",
+        )
 
-# Parse the arguments
-args = parser.parse_args()
+    # Parse the arguments
+    return parser.parse_args()
 
-def parse_categories():
+def parse_categories(args):
     upper_body = args.upper_body
     lower_body = args.lower_body
     full_body = args.full_body
@@ -42,7 +50,7 @@ def parse_categories():
         upper_body = True
         lower_body = True
         full_body = True
-    categories = []
+    categories = ["Stretching"]
     if upper_body:
         categories.append("Upper")
 
@@ -54,30 +62,43 @@ def parse_categories():
 
     return categories
 
-categories = parse_categories()
+args = get_args()
+categories = parse_categories(args)
 data = get_data()
-data['exercises'].sort(key=lambda x:x['lastComplete'])
+
+#data['exercises'].sort(key=lambda x:x['lastComplete'])
+data['exercises'].sort(key=lambda x:random.randint(1, 100000))
 
 free_weights = []
 machines = []
 aerobics = []
+stretching = []
+
+NUM_FREEWEIGHTS=4
+NUM_MACHINES=4
+NUM_AEROBICS=4
+NUM_STRETCHING=2
 
 for item in data['exercises']:
-    if not item['category'] in categories:
-        continue
-    if item['type'] == "Aerobics" and len(aerobics) < 2:
-        aerobics.append(item)
-    elif item['type'] == "Free Weights" and len(free_weights) < 2:
-        free_weights.append(item)
-    elif item['type'] == "Machines" and len(machines) < 2:
-        machines.append(item)
+    try:
+        if not item['category'] in categories:
+            continue
+        if item['name'] in exclude:
+            continue
+        if item['type'] == "Aerobics" and len(aerobics) < NUM_AEROBICS:
+            aerobics.append(item)
+        elif item['type'] == "Free Weights" and len(free_weights) < NUM_FREEWEIGHTS:
+            free_weights.append(item)
+        elif item['type'] == "Machines" and len(machines) < NUM_MACHINES:
+            machines.append(item)
+        elif item['type'] == "Stretching" and len(stretching) < NUM_STRETCHING:
+            stretching.append(item)
+    except:
+        print("Error processing: " + str(item))
 
 def print_item(item):
     print("{}: {} / {}".format(item['name'], item['hypertrophy'], item['tone']))
 
-for item in free_weights:
-    print_item(item)
-for item in machines:
-    print_item(item)
-for item in aerobics:
-    print_item(item)
+for x in [stretching, free_weights, machines, aerobics]:
+    for item in x:
+        print_item(item)
